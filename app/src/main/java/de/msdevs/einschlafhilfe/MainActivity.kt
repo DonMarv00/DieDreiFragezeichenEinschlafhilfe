061a51dce73a0103fc1c42e1a7621b094e3132e8
Marvin Stelter <41212885+MarvinStelter@users.noreply.github.com>
2022-10-05 23:17:08 +0200

    Fehler behoben Folgen aktualiesiert Ungenutzten Code entfernt

--- app/src/main/java/de/msdevs/einschlafhilfe/MainActivity.kt
+++ app/src/main/java/de/msdevs/einschlafhilfe/MainActivity.kt
@@ -8,7 +8,6 @@ import android.content.pm.PackageManager
 import android.net.ConnectivityManager
 import android.net.Uri
 import android.os.Bundle
-import android.util.Log
 import android.view.Menu
 import android.view.MenuItem
 import android.view.View
